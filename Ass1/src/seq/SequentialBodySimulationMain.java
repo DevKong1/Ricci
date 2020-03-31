08c6734697f129f04693cc047efc49c1927428a5
Luca Salvigni <luca.salvigni7@studio.unibo.it>
2020-03-31 18:42:01 +0200

    Create new class SimulatorAgent

--- Ass1/src/seq/SequentialBodySimulationMain.java
+++ Ass1/src/seq/SequentialBodySimulationMain.java
@@ -6,13 +6,29 @@ package pcd.ass01.seq;
  * @author aricci
  */
 public class SequentialBodySimulationMain {
+	
+	private static final int nBodies = 10;
+	private static final int nSteps = 30;
+	private static int nThreads;
 
     public static void main(String[] args) {
-                
-    	SimulationViewer viewer = new SimulationViewer(620,620);
-
+    	
+    	if(Runtime.getRuntime().availableProcessors() +1 > 3) {
+    		nThreads = Runtime.getRuntime().availableProcessors() + 1;
+    	} else {
+    		nThreads = 3;
+    	}
+    	
+    	int i = 0;
+    	while(i < nThreads-2) {
+    		new SimulatorAgent(nBodies, nSteps, nThreads-2).start();
+    		i++;
+    	}
+    	
+    	/*SimulationViewer viewer = new SimulationViewer(620,620);
+        
     	Simulator sim = new Simulator(viewer);
-        sim.execute();
+        sim.execute();*/
 
     }
 }
