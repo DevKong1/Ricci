1c33f5cb9ce63c95b5c98a212eb91ea8baa37179
Manuele Pasini <manuele.pasini@studio.unibo.it>
2020-04-01 16:16:17 +0200

    Now threads solve collisions between balls handled to them

--- Ass1/src/seq/Worker.java
+++ Ass1/src/seq/Worker.java
@@ -33,12 +33,12 @@ public class Worker extends Thread {
 			context.lockUpdateSem();
 			// DEBUG : Old x position print
 			/*System.out.println("I'm thread " +this.getName()+" Before Updating Pos");
-			int k = 0;
+			int z = 0;
 			for(Body b : context.getBallList()){
 				
 				System.out.print(i++ + " " +b.getPos().getX()+" - ");
 			}
-			i=0;
+			z=0;
 			System.out.println("\n");  */
 			context.getBallList().subList(start, lastIndex).forEach(x -> x.updatePos(dt));
 			//DEBUG :  new x positions
@@ -47,23 +47,39 @@ public class Worker extends Thread {
 				
 				System.out.print(i++ + " " +b.getPos().getX()+" - ");
 			}
-			k=0;
+			z=0;
 			System.out.println("\n");  */
 			context.releaseUpdateSem();		
 			context.waitNonConcurrentCalc();
+			
+			context.lockUpdateSem();
+			checkInternalCollisions();
+			context.releaseUpdateSem();	
+			context.waitNonConcurrentCalc();
+			
 			// System.out.println()
 			threadBalls.stream().forEach(x -> x.checkAndSolveBoundaryCollision(context.getBounds()));
 			context.waitNonConcurrentCalc();
 		}
 
 	}
-
-	private void log(String msg) {
+	
+	private void log(final String msg) {
 		synchronized (System.out) {
 			System.out.println("[ " + getName() + " ] " + msg);
 		}
 	}
-
+	private void checkInternalCollisions(){
+		for (int k = start; k < lastIndex - 1; k++) {
+	    	Body b1 = context.getBallList().get(k);
+	        for (int j = k + 1; j < lastIndex; j++) {
+	        	Body b2 = context.getBallList().get(j);
+	            if (b1.collideWith(b2)) {		        
+	            	Body.solveCollision(b1, b2);		            	
+	            }
+	        }
+        }
+	}
 	private void waitFor(long ms) throws InterruptedException {
 		Thread.sleep(ms);
 	}
