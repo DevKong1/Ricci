3bd0122160c2dec8dea8db99b7552b966e6716b4
Luca Salvigni <luca.salvigni7@studio.unibo.it>
2020-04-04 18:16:25 +0200

    Deleted GUI

--- Ass1/src/seq/BouncingBalls.java
+++ Ass1/src/seq/BouncingBalls.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-
 public class BouncingBalls {
 
 	public static void main(String[] args) {
@@ -23,13 +22,6 @@ public class BouncingBalls {
 			perThread = context.getBallsPerThread();
 			workers.add(new Worker("Worker-" + i, nStep, context, tmp, tmp+=perThread));
 		}
-		ModelGUI model = new ModelGUI();
-		ControllerGUI controller = new ControllerGUI(model);
-	    ViewGUI view = new ViewGUI(controller);
-	    model.addObserver(view);    
-	    view.setVisible(true);
-	    
-	    new AgentGUI(model).start();
 
 		for (Worker w : workers) {
 			w.start();
--- Ass1/src/seq/ModelObserver.java
+++
@@ -1,6 +0,0 @@
-package src.seq;
-
-public interface ModelObserver {
-
-	void modelUpdated(ModelGUI model);
-}
