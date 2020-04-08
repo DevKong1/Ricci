a2f1d98d0adf799141c0ada24e114dd2c34110bc
Luca Salvigni <luca.salvigni7@studio.unibo.it>
2020-04-08 16:05:59 +0200

    Refactor class Game

--- Ass1/src/guiVersion/Game.java
+++ Ass1/src/guiVersion/Game.java
@@ -1,14 +1,12 @@
 package src.guiVersion;
 
+//Main class of the project
 public class Game {
 
 	public static void main(String[] args) {
-		//int threads = 4;
 		int steps = 5000;
 		int balls = 1000;
 		new BouncingBalls(balls,steps).init();
-				
-
 	}
 
 }
